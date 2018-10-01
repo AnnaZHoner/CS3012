32d9df2eec095cd204f2cc6b36284168e51c5522
AnnaZHoner <honera@tcd.ie>
2018-10-01 11:12:28 +0100

    Added testing framework

---
+++ LCA/Test1/unittest1.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include "LCA.cpp"
+#include "pch.h"
+#include "CppUnitTest.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Test1
+{		
+	TEST_CLASS(UnitTest1) 
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int 1;
+			Node * newNode(int k);
+
+
+		}
+
+	};
+}
