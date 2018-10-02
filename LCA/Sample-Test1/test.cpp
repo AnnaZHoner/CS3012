d673e9f48075913de3a9ae37c330c7562ebff8b3
AnnaZHoner <honera@tcd.ie>
2018-10-02 13:22:26 +0100

    Cleaned code

--- LCA/Sample-Test1/test.cpp
+++ LCA/Sample-Test1/test.cpp
@@ -1,14 +1,23 @@
 #include "pch.h"
-#include "../LCA/LCA.h"
+#include "../LCA.h"
 
 
 
-TEST(Test1, TestFullGraph) 
+TEST(Test1, TestFullGraph)
 {
 
-	Node * root = newNode(0);
+	Node * root = newNode(1);
 	root->left = newNode(2);
-	
-	// EXPECT_EQ(findLCA(root, 4, 5), 2);
-	// EXPECT_TRUE(true);
+	/*root->right = newNode(3);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
+	root->right->left = newNode(6);
+	root->right->right = newNode(7);
+
+
+	EXPECT_EQ(findLCA(root, 4, 5), 2);
+	//EXPECT_TRUE(true);
+
+	*/
+
 }
