0116c08cf370f10bd8dc2af67df1a755e178b679
AnnaZHoner <35963798+AnnaZHoner@users.noreply.github.com>
2018-11-30 09:41:28 +0000

    Moved to separate directory

--- gitaccess.js
+++
@@ -1,8 +0,0 @@
-var github = require('octonode');
-var client = github.client();
-
-
-client.get('/users/AnnaZHoner', {}, function (err, status, body, headers) {
-  console.log(body); //json object
-});
-
